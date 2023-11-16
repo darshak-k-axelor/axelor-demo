b533803f6eac0a2302fc6446c25f98fa8048711a
darshak <d.kachchhi@axelor.com>
2023-11-16 16:42:37 -0500

    Added Models for account and accountEntry in axelor-acconting module

--- modules/axelor-invoicing/src/main/java/com/axelor/apps/invoicing/module/InvoicingModuleConfiguration.java
+++ modules/axelor-invoicing/src/main/java/com/axelor/apps/invoicing/module/InvoicingModuleConfiguration.java
@@ -1,7 +1,9 @@
 package com.axelor.apps.invoicing.module;
 
 import com.axelor.app.AxelorModule;
+import com.axelor.apps.invoicing.db.repo.OrderManagementRepository;
 import com.axelor.apps.invoicing.service.*;
+import com.axelor.apps.sales.db.repo.OrderRepository;
 
 public class InvoicingModuleConfiguration  extends AxelorModule {
     @Override
@@ -9,6 +11,7 @@ public class InvoicingModuleConfiguration  extends AxelorModule {
         bind(OrderService.class).to(OrderServiceImpl.class);
         bind(InvoiceService.class).to(InvoiceServiceImpl.class);
         bind(InvoiceLineService.class).to(InvoiceLineServiceImpl.class);
+        bind(OrderRepository.class).to(OrderManagementRepository.class);
 
     }
 
