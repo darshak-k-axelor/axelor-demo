7dc1e78555fbac5e0775763102218f5c551e49f3
darshak <d.kachchhi@axelor.com>
2023-11-20 14:48:36 -0500

    Override Order service to add account in axelor-acconting module

--- modules/axelor-accounting/src/main/java/com/axelor/apps/accounting/module/AccountingModuleConfiguration.java
+++ modules/axelor-accounting/src/main/java/com/axelor/apps/accounting/module/AccountingModuleConfiguration.java
@@ -1,18 +1,18 @@
 package com.axelor.apps.accounting.module;
 
 import com.axelor.app.AxelorModule;
-import com.axelor.apps.accounting.service.InvoiceAccountingService;
-import com.axelor.apps.accounting.service.InvoiceAccountingServiceImpl;
-import com.axelor.apps.accounting.service.InvoiceLineAccountingService;
-import com.axelor.apps.accounting.service.InvoiceLineAccountingServiceImpl;
+import com.axelor.apps.accounting.service.*;
 import com.axelor.apps.invoicing.service.InvoiceLineService;
 import com.axelor.apps.invoicing.service.InvoiceLineServiceImpl;
+import com.axelor.apps.invoicing.service.OrderServiceImpl;
 
 public class AccountingModuleConfiguration extends AxelorModule {
     @Override
     protected void configure() {
         bind(InvoiceAccountingService.class).to(InvoiceAccountingServiceImpl.class);
-        bind(InvoiceLineAccountingService.class).to(InvoiceLineAccountingServiceImpl.class);
+        bind(OrderServiceImpl.class).to(OrderAccountingServiceImpl.class);
+
+
 
     }
 
--- modules/axelor-accounting/src/main/java/com/axelor/apps/accounting/service/OrderAccountingServiceImpl.java
+++ modules/axelor-accounting/src/main/java/com/axelor/apps/accounting/service/OrderAccountingServiceImpl.java
@@ -1,8 +1,9 @@
-package com.axelor.apps.invoicing.service;
+package com.axelor.apps.accounting.service;
 
 import com.axelor.apps.invoicing.db.Invoice;
 import com.axelor.apps.invoicing.db.InvoiceLine;
 import com.axelor.apps.invoicing.db.repo.InvoiceRepository;
+import com.axelor.apps.invoicing.service.OrderServiceImpl;
 import com.axelor.apps.sales.db.Order;
 import com.axelor.apps.sales.db.OrderLine;
 import com.axelor.apps.sales.db.repo.OrderRepository;
@@ -17,14 +18,11 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class OrderServiceImpl implements OrderService{
-    private static final Logger LOG = LoggerFactory.getLogger(OrderServiceImpl.class);
-    public static final int FETCH_LIMIT = 20;
-    protected OrderRepository orderRepository;
-    protected InvoiceRepository invoiceRepository;
+public class OrderAccountingServiceImpl extends OrderServiceImpl {
 
     @Inject
-    public OrderServiceImpl(OrderRepository orderRepository, InvoiceRepository invoiceRepository) {
+    public OrderAccountingServiceImpl(OrderRepository orderRepository, InvoiceRepository invoiceRepository) {
+        super(orderRepository, invoiceRepository);
         this.orderRepository = orderRepository;
         this.invoiceRepository = invoiceRepository;
     }
@@ -32,13 +30,7 @@ public class OrderServiceImpl implements OrderService{
     @Override
     @Transactional(rollbackOn = {Exception.class})
     public void generateInvoiceForOrder(Order currentOrder) {
-
         currentOrder = orderRepository.find(currentOrder.getId());
-
-//        ObjectMapper mapper = new ObjectMapper();
-//        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-//        Invoice currentInvoice = mapper.convertValue(currentOrder, Invoice.class);
-
         Invoice currentInvoice =new Invoice();
 
         List<OrderLine> orderLineList = currentOrder.getOrderLineList();
@@ -53,6 +45,7 @@ public class OrderServiceImpl implements OrderService{
         currentInvoice.setExTaxTotal(currentOrder.getExTaxTotal());
         currentInvoice.setTotal(currentOrder.getTotal());
         currentInvoice.setStateSelect(OrderRepository.STATUS_DRAFT);
+        currentInvoice.setCustomerAccount(currentOrder.getCustomerAccount());
 
         currentOrder.setBillingDate(LocalDate.now());
         currentOrder.setInvoice(currentInvoice);
@@ -65,11 +58,6 @@ public class OrderServiceImpl implements OrderService{
     private List<InvoiceLine> convertOrderLineListToInvoiceLineList(List<OrderLine> orderLineList, Invoice invoice) {
         List<InvoiceLine> resultInvoiceLineList = new ArrayList<>();
         for (OrderLine orderLine : orderLineList) {
-
-//            ObjectMapper mapper = new ObjectMapper();
-//            mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-//            InvoiceLine invoiceLine = mapper.convertValue(orderLine, InvoiceLine.class);
-//            System.out.println(orderLine);
             InvoiceLine invoiceLine = new InvoiceLine();
             invoiceLine.setInvoice(invoice);
             invoiceLine.setProduct(orderLine.getProduct());
@@ -79,40 +67,10 @@ public class OrderServiceImpl implements OrderService{
             invoiceLine.setExTaxTotal(orderLine.getExTaxTotal());
             invoiceLine.setTaxRate(orderLine.getTaxRate());
             invoiceLine.setTotal(orderLine.getTotal());
+            invoiceLine.setInvoiceLineAccount(orderLine.getProduct().getProductAccount());
+
             resultInvoiceLineList.add(invoiceLine);
         }
         return resultInvoiceLineList;
-    }
-
-
-
-    @Override
-    @Transactional(rollbackOn = {Exception.class})
-    public int generateLateOrderInvoicesForOrder() {
-        int totalInvoiceGenerated = 0;
-        Query<Order> query =
-                orderRepository
-                        .all()
-                        .filter("self.invoice = null AND self.forecastBillingDate < :currentDate")
-                        .bind("currentDate", LocalDate.now());
-
-        List<Order> lateOrderList = query.fetch(FETCH_LIMIT);
-
-        while (!lateOrderList.isEmpty()) {
-            totalInvoiceGenerated = totalInvoiceGenerated + lateOrderList.size();
-            generateLateOrderInvoicesForOrder(lateOrderList);
-
-            JPA.clear();
-            lateOrderList = query.fetch(FETCH_LIMIT);
-        }
-
-        return totalInvoiceGenerated;
-
-    }
-
-    private void generateLateOrderInvoicesForOrder(List<Order> lateOrderList){
-        for (Order lateOrder : lateOrderList) {
-            generateInvoiceForOrder(lateOrder);
-        }
     }
 }
